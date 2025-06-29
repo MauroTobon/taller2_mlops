4b0d1fe9bcda0ed2a89d43c40dd571556e4e3e81
MauroTobon <65326358+MauroTobon@users.noreply.github.com>
2025-06-28 19:11:22 -0500

    Se corrige código main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-!pip install fastapi uvicorn nest-asyncio pyngrok
-
-from pyngrok import ngrok
-
-# Conectar ngrok con tu cuenta
-ngrok.set_auth_token("2z7fXSKiBmQzjRsVF4NwbkEPzfz_3gp8C1ZDPHUcX1U5BLoRM")
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
 import nest_asyncio
-from pyngrok import ngrok
-import uvicorn
 
 # Aplicar parche para correr async event loop en Colab
 nest_asyncio.apply()
@@ -42,10 +33,3 @@ def predict(data: InputData):
     df = pd.DataFrame([data.dict()])
     pred = modelo.predict(df)
     return {"prediction": int(pred[0])}
-
-# Abrir túnel público a Colab
-public_url = ngrok.connect(8000)
-print("Tu API está disponible en:", public_url)
-
-# Ejecutar servidor
-uvicorn.run(app, host="0.0.0.0", port=8000)
